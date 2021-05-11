cd81ae472e6cb80c0074ce37520c478371a72bd9
Govardhan Rao kadimi <govardhan.kadimi@gmail.com>
2021-05-11 10:03:36 +0530

    edabit_challenges_06.js

---
+++ edabit_challenges_07.js
@@ -0,0 +1,5 @@
+//Create a function that finds the maximum range of a triangle's third edge, where the side lengths are all integers.
+
+function nextEdge(side1, side2) {
+	return (side1 + side2) - 1;
+}
