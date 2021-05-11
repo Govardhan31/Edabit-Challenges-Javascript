5cf0db263da3865bc496ae70174310dd4a53f261
Govardhan Rao kadimi <govardhan.kadimi@gmail.com>
2021-05-11 11:16:57 +0530

    Is the Last Character an "N"?

---
+++ edabit_challenges_34.js
@@ -0,0 +1,6 @@
+//Create a function that takes a string (a random name). If the last character of the name is an "n", return true, otherwise return false.
+
+
+function isLastCharacterN(word) {
+	return word.endsWith('n');
+}
