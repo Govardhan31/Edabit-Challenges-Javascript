0179bcbf2b6291e75cf9c2692fa4b43541bf6cba
Govardhan Rao kadimi <govardhan.kadimi@gmail.com>
2021-05-11 11:29:34 +0530

    Name Greeting!

---
+++ edabit_challenges_37.js
@@ -0,0 +1,6 @@
+/*Create a function that takes a name and returns a greeting in the form of a string.
+ Don't use a normal function, use an arrow function.*/
+
+
+
+const helloName = name => "Hello"+" "+name+"!";
