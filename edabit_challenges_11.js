b98850afa07bdcb23b8442ce49cdacffacde6151
Govardhan Rao kadimi <govardhan.kadimi@gmail.com>
2021-05-11 10:14:35 +0530

    Return Something to Me!

---
+++ edabit_challenges_11.js
@@ -0,0 +1,7 @@
+//Write a function that returns the string "something" joined with a space " " and the given argument a.
+
+
+function giveMeSomething(a) {
+	var re="something"
+	return re+" "+a;
+}
