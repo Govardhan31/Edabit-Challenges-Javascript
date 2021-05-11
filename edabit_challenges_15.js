4af5cd42f36376ae6453100e6c59985842e76684
Govardhan Rao kadimi <govardhan.kadimi@gmail.com>
2021-05-11 10:27:23 +0530

    Football Points

---
+++ edabit_challenges_15.js
@@ -0,0 +1,6 @@
+/*Create a function that takes the number of wins, draws and losses and 
+calculates the number of points a football team has obtained so far.*/
+
+function footballPoints(wins, draws, losses) {
+		return wins*3 + draws;
+}
