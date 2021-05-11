7c1f277516d0cf8c23a2da06c0f9546ccbb27884
Govardhan Rao kadimi <govardhan.kadimi@gmail.com>
2021-05-11 09:59:46 +0530

    Convert Minutes into Seconds

--- edabit_challenges_03.js
+++ edabit_challenges_03.js
@@ -1,4 +1,4 @@
-//Write a function that takes an integer minutes and converts it to seconds.
+//Write a function that takes an integer minutes and converts it to seconds..
 
 function convert(minutes) {
 	return 60*minutes;
