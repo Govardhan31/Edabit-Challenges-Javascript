d3c9655036ced1788fb94196e4a572072a3b03c1
Govardhan Rao kadimi <govardhan.kadimi@gmail.com>
2021-05-11 10:43:08 +0530

    Correct the Mistakes

---
+++ edabit_challenges_20.js
@@ -0,0 +1,18 @@
+function squaed(b) {
+	return a * a
+}
+
+
+
+/*Fix the code in the code tab to pass this challenge (only syntax errors). 
+Look at the examples below to get an idea of what the function should do.*/
+
+
+
+
+
+
+
+function squared(b) {
+	return b*b;
+}
