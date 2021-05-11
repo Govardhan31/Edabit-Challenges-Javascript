976e1bd4daaf796913c6163ecda9ff3db58fca16
Govardhan Rao kadimi <govardhan.kadimi@gmail.com>
2021-05-11 11:26:39 +0530

    Find the Index

---
+++ edabit_challenges_36.js
@@ -0,0 +1,7 @@
+/*Create a function that searches for the index of a given item in an array.
+ If the item is present, it should return the index, otherwise, it should return -1.*/
+
+
+function search(arr, item) {
+	return arr.indexOf(item);
+}
