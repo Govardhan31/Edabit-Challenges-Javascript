dfe80532115a3782215398f67be7657753051787
Govardhan Rao kadimi <govardhan.kadimi@gmail.com>
2021-05-13 08:42:55 +0530

    Buggy Code 1

---
+++ edabit_challenges_43.js
@@ -0,0 +1,15 @@
+function cubes(a) {
+	retunr a ** 3
+}
+
+
+
+/*Fix the code in the code tab to pass this challenge (only syntax errors). 
+Look at the examples below to get an idea of what the function should do.*/
+
+
+
+
+function cubes(a) {
+	return a ** 3
+}
